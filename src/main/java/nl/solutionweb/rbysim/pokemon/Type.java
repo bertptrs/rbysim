38a5703929cda906acd22f8e7dcf7d70ec8152a5
Bert Peters <bert.ljpeters@gmail.com>
2015-11-24 11:39:22 +0100

    Start implementing in Java.

---
+++ src/main/java/nl/solutionweb/rbysim/pokemon/Type.java
@@ -0,0 +1,24 @@
+package nl.solutionweb.rbysim.pokemon;
+
+/**
+ * Different types to be had.
+ *
+ * @author Bert Peters
+ */
+public enum Type {
+    NORMAL,
+    FIGHTING,
+    FLYING,
+    POISON,
+    GROUND,
+    ROCK,
+    BUG,
+    GHOST,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    PSYCHIC,
+    ICE,
+    DRAGON
+}
